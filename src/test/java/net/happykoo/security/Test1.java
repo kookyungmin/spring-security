c40a7f9bde565752ceb63ca914bb5dc0c5ebc067
gguri4549 <marco@planit.ai>
2024-10-29 11:25:39 +0900

    feat: project init

---
+++ src/test/java/net/happykoo/security/Test1.java
@@ -0,0 +1,12 @@
+package net.happykoo.security;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Test1 {
+    @Test
+    void test1() {
+        assertEquals("TT", "TT");
+    }
+}
