c177e9a0705f39c4f0f936db5811117cb1fa8606
gguri4549 <marco@planit.ai>
2024-11-05 10:48:47 +0900

    feat: ch1

--- src/main/java/net/happykoo/security/config/SecurityConfig.java
+++ src/main/java/net/happykoo/security/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,46 +16,46 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity(debug = true)
 //prePost로 권한 설정 작동
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        //기본적으로 셋팅되어 있음
-//        http.authorizeRequests((requests) -> {
-//           requests.antMatchers("/ch1/").permitAll()
-//                   .anyRequest().authenticated();
-//        });
-//
-//        http.formLogin();
-//        http.httpBasic();
-//
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer() {
-//        return (web) -> web.ignoring().requestMatchers();
-//    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //기본적으로 셋팅되어 있음
+        http.authorizeRequests((requests) -> {
+           requests.antMatchers("/test/").permitAll()
+                   .anyRequest().authenticated();
+        });
 
-//    @Bean
-//    public InMemoryUserDetailsManager userDetailsManager() {
-//        UserDetails user = User.builder()
-//                .username("user1")
-//                .password(passwordEncoder().encode("1111"))
-//                .roles("USER")
-//                .build();
-//
-//        UserDetails admin = User.builder()
-//                .username("user2")
-//                .password(passwordEncoder().encode("1111"))
-//                .roles("ADMIN")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(user, admin);
-//    }
+        http.formLogin();
+        http.httpBasic();
 
-//    @Bean
-//    PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails user = User.builder()
+                .username("user1")
+                .password(passwordEncoder().encode("1111"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("user2")
+                .password(passwordEncoder().encode("1111"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
